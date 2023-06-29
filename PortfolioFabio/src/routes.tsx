1a6ef5255a2ec4ef87ef687cec07776974550fe6
Fabio Luiz <fabioeeereed@gmail.com>
2023-06-29 18:17:48 -0300

    menosm

--- PortfolioFabio/src/routes.tsx
+++ PortfolioFabio/src/routes.tsx
@@ -9,7 +9,7 @@ export const MyRoutes = () => {
     <UserProvider>
       <Routes>
         <Route path='/' element={ <Home /> } />
-        <Route path='/projects' element={<Projects />}  />
+        <Route path='/projects' element={<Projects />} />
         <Route path='/about' element={ <AboutMe /> } />
       </Routes>
     </UserProvider>
